47bb98d28aaceeaddf35b01efccca109361234ee
kunduvikram <kunduvikram@gmail.com>
2015-08-04 23:27:43 +0530

    Hello
    First commit

---
+++ app.js
@@ -0,0 +1,23 @@
+﻿var app = angular.module('app', ['ngRoute', 'ngAnimate']);
+
+app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+    $routeProvider.when('/Assess',
+                        {
+                            templateUrl: 'Home/Assess',
+                            controller: 'assessController'
+                        });
+    $routeProvider.when('/Plan',
+                       {
+                           templateUrl: 'Home/Plan',
+                           controller: 'planController'
+                       });
+    $routeProvider.when('/Review',
+                      {
+                          templateUrl: 'Home/Review',
+                          controller: 'reviewController'
+                      });
+    $routeProvider.otherwise(
+                   {
+                       redirectTo: '/'
+                   });
+}]);
